eff4eb1760dbb902b6c8dfdc901e7860401ebdc4
38147504 <38147504@L815WD220032.edu.cbe.ab.ca>
2025-09-15 13:11:35 -0600

    1:11

--- Chapter3/src/SkillBuilders/Didgets.java
+++ Chapter3/src/SkillBuilders/Didgets.java
@@ -1,5 +1,23 @@
 package SkillBuilders;
 
-public class Didgets {
+import java.util.Scanner;
 
+public class Didgets {
+    public static void main(String[] args) {
+    	Scanner scanner = new Scanner(System.in);
+    	 
+    	System.out.print("Enter a two-digit number: ");
+    	int number = scanner.nextInt();
+    	
+    	//Get tens place number
+    	int tens = number/10;
+    	
+    	//Get ones place digit
+    	int ones = number%10;
+    	
+    	System.out.println("Tens place digit:" + tens);
+        System.out.println("Ones place digit: " + ones);
+
+        scanner.close();
+    }
 }
