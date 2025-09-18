a2bd502598537ba356d0ccd24c59a56d05273979
38147504 <38147504@L815WD220032.edu.cbe.ab.ca>
2025-09-18 14:40:32 -0600

    2:40

--- Chapter3/src/SkillBuilders/GradeAvgP1.java
+++ Chapter3/src/SkillBuilders/GradeAvgP1.java
@@ -6,25 +6,25 @@ public class GradeAvgP1 {
 
     public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner (System.in);
+		Scanner input = new Scanner (System.in);
 		
 		System.out.print("Enter grade 1:");
-		double grade1 = scanner.nextDouble();
+		double grade1 = input.nextDouble();
 		
 		System.out.print("Enter grade 2:");
-		double grade2 = scanner.nextDouble();
+		double grade2 = input.nextDouble();
 		
 		System.out.print("Enter grade 3:");
-		double grade3 = scanner.nextDouble();
+		double grade3 = input.nextDouble();
 		
 		System.out.print("Enter grade 4:");
-		double grade4 = scanner.nextDouble();
+		double grade4 = input.nextDouble();
 		
 		double average = (grade1 + grade2 + grade3 + grade4) / 4;
 		
 		System.out.printf("The average grade is: %.2f\n", average);
 		
-		scanner.close();
+		input.close();
 	}
 
 }
